06c6e6a83325482f356ecd94cb24d2fa45217473
wnyl_xx <hyy96hyy@naver.com>
2025-02-18 13:52:47 +0900

    Temporary server function completion

--- TCPServer.js
+++ TCPServer.js
@@ -12,7 +12,22 @@ const tcpServer = net.createServer((socket) => {
         const packet = parsePacket(data);
 
         // 체크섬 확인
-        
+        if (checkChecksum(packet)) {
+            console.log('체크섬 확인됨');
+
+            // 명령 처리
+            handleCommand(packet);
+
+            // 클라이언트에 명렁 처리 완료 메시지 전송
+            socket.write('명령 처리 완료');
+        } else {
+            console.log('체크섬 오류');
+            socket.write('체크섬 오류');
+        }
+    });
+
+    socket.on('end', () => {
+        console.log('클라이언트 연결 종료');
     });
 
 });
@@ -41,22 +56,62 @@ function checkChecksum(packet) {
 
 // 명령 처리 함수
 function handleCommand(packet) {
-
+    switch (packet.commandCode) {
+        case 1:
+            handlePanTilt(packet);
+            break;
+        case 2:
+            handleZoom(packet);
+            break;
+        case 3:
+            handleVolume(packet);
+            break;
+        default:
+            console.log('알 수 없는 명령')
+    }
 }
 
 
 // 팬틸트 명령 처리 함수
 function handlePanTilt(packet) {
-
+    if (packet.action === 1) {
+        panTilt.height += 1;
+        console.log(`팬틸트 높이값 +1: ${panTilt.height}`);
+    } else if (packet.action === -1) {
+        panTilt.height -= 1;
+        console.log(`팬틸트 높이값 -1: ${panTilt.height}`);
+    } else if (packet.action === 2) {
+        panTilt.position += 1;  // 우 방향
+        console.log(`팬틸트 위치값 +1: ${panTilt.position}`);
+    } else if (packet.action === -2) {
+        panTilt.position -= 1;  // 좌 방향
+        console.log(`팬틸트 위치값 -1: ${panTilt.position}`);
+    } else {
+        console.log("잘못된 팬틸트 명령")
+    }
 }
 
 
 // 줌 명령 처리 함수
 function handleZoom(packet) {
-
+    if (packet.action === 1 && zoomLevel <= 0.9){
+        zoomLevel += 0.1;
+        console.log(`줌 +1: ${zoomLevel}`);
+    } else if (packet.action === -1 && zoomLevel >= 0.1) {
+        zoomLevel -= 0.1;
+        console.log(`줌 -1: ${zoomLevel}`);
+    } else {
+        console.log("잘못된 줌 명령")
+    }
 }
 
 // 볼륨 명령 처리 함수
 function handleVolume(packet) {
-    
+    if(packet.action === 1 && volumeLevel < 100) {
+        volumeLevel += 1;
+        console.log(`볼륨 +1: ${volumeLevel}`);
+    } else if (packet.action === 1 && volumeLevel > 0) {
+        volumeLevel -= 1;
+        console.log(`볼륨 -1: ${volumeLevel}`);
+    }
 }
